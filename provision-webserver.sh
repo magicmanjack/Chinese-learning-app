50a542bb31294614f867246823c12fc127d8a1c2
magicalmanjack <magicalmanjack@gmail.com>
2025-08-24 17:05:35 +1200

    Added serving of website

---
+++ provision-webserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y apache2 libapache2-mod-php php php-mysql
+
+cp /vagrant/website.conf /etc/apache2/sites-available/
+
+a2ensite website
+a2dissite 000-default
+
+service apache2 restart
